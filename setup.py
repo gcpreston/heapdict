2b36c9d40efaf4defe254863eabb53269d59234e
Graham Preston <preston.gr@husky.neu.edu>
2019-01-03 11:44:14 -0500

    Change module name and style

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ import sys
 if sys.version_info[0] <= 2:
     import ez_setup
     ez_setup.use_setuptools()
-    from setuptools import setup, Extension
+    from setuptools import setup
 else:
-    from distutils.core import setup, Extension
+    from distutils.core import setup
 
-setup(name='HeapDict',
+setup(name='heapdict',
       version='1.0.0',
       description='a heap with decrease-key and increase-key operations',
       author='Stutzbach Enterprises, LLC',
       author_email='daniel@stutzbachenterprises.com',
       url='http://stutzbachenterprises.com/',
-      license = "BSD",
-      keywords = "heap decrease-key increase-key dictionary Dijkstra A* priority queue",
-      provides = ['heapdict'],
-      py_modules = ['heapdict'],
-      test_suite = "test_heap",
-      zip_safe = True,
-      classifiers = [
+      license="BSD",
+      keywords="heap decrease-key increase-key dictionary Dijkstra A* priority queue",
+      provides=['heapdict'],
+      py_modules=['heapdict'],
+      test_suite="test_heap",
+      zip_safe=True,
+      classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Operating System :: OS Independent',
           'Intended Audience :: Developers',
